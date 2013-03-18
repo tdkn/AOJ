52c3608b7326216b7b8d002b553dc72127848a22
tdkn <web@tedokon.com>
2013-03-18 23:09:45 +0900

    add new problem.

---
+++ aoj10012.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+
+class Rectangle
+  def initialize(height,width)
+    @h = height
+    @w = width
+  end
+  def print_rect
+    @h.times do |i|
+      @w.times do |j|
+        print "#"
+      end
+      print "\n"
+    end
+    print "\n"
+  end
+end
+
+rectangles = Array.new
+
+while line = gets
+  line.chomp!
+  break if line.split.size != 2
+  h,w = line.split.map! {|n| n.to_i }
+  if [h,w] == [0,0] then
+    rectangles.each do |rectangle|
+      rectangle.print_rect
+    end
+    break
+  end
+  rectangles.push Rectangle.new(h,w)
+end

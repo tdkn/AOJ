f6c63a3175748f07445754e6fc5733be373563a7
tdkn <web@tedokon.com>
2013-03-23 02:52:24 +0900

    modified.

--- aoj10016.rb
+++ aoj10016.rb
@@ -1,53 +1,46 @@
 #!/usr/bin/env ruby
 # -*- coding: utf-8 -*-
 
-class Test
+class Exam
   def initialize
     # middle final retest
     @score = {:m => 0, :f => 0, :r => 0}
-    @grade = "X"
   end
 
-  attr_accessor :score, :grade
+  def mf_total
+    @score[:m] + @score[:f]
+  end
+
+  def absence?
+    @score[:m] == -1 || @score[:f] == -1
+  end
 
   def add_score(scores)
-    @score[:m] = scores[0].to_i
-    @score[:f] = scores[1].to_i
-    @score[:r] = scores[2].to_i
-    @grade = judge_grade
+    @score[:m],@score[:f],@score[:r] = scores
   end
 
-  private
-  def judge_grade
-    score_mf = @score[:m] + @score[:f]
-    if (score_mf >= 80) then
-      return "A"
-    elsif (65 <= score_mf && score_mf < 80) then
-      return "B"
-    elsif (50 <= score_mf && score_mf < 65) then
-      return "C"
-    elsif (30 <= score_mf && score_mf < 50 && @score[:m] != -1 && @score[:f] != -1) then
-      return "C" if @score[:r] >= 50
-      return "D"
-    elsif (score_mf < 30 || @score[:m] == -1 || @score[:f] == -1) then
-      return "F"
+  def grade
+    return "F" if absence? || mf_total < 30
+    case mf_total
+    when 80..100 then "A"
+    when 65..80  then "B"
+    when 50..65  then "C"
+    when 30..65  then (@score[:r] >= 50) ? "C" : "D"
     end
   end
 end
 
-score_table = Array.new
+student_table = Array.new
 
 while
   input = gets.chomp
 
   break if input == "-1 -1 -1"
 
-  result = Test.new
-  result.add_score(input.split)
-  score_table.push(result)
+  exam = Exam.new
+  exam.add_score(input.split.map(&:to_i))
+  student_table.push(exam)
 end
 
-score_table.each do |item|
-  puts item.grade
-end
+puts student_table.map(&:grade)
 
